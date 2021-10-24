0e4a877566ff47ac8145bfc8da9710f32538789e
jw5367 <zzss9921@naver.com>
2021-10-24 18:51:10 +0900

    Create weather

---
+++ JS/weather.js
@@ -0,0 +1,19 @@
+const API_KEY = "9f7004fc009ed418c4bc0377c7816bff";
+
+function onGeoOk(position) {
+const lat = position.coords.latitude;
+const lon = position.coords.longitude;
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+fetch(url)
+.then((response) => response.json())
+.then((data) =>{
+  const weather = document.querySelector("#weather span:first-child");
+  const city = document.querySelector("#weather span:last-child");
+  city.innerText = data.name;
+  weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+});
+}
+function onGeoError() {
+  console.log("Can't find you. No weather for you.");
+}
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
