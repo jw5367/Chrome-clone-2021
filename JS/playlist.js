6d792feb6231ba3df1fd0850d1aa557e23f7e0de
jw5367 <zzss9921@naver.com>
2021-10-31 18:10:45 +0900

    final

--- JS/playlist.js
+++
@@ -1,8 +0,0 @@
-const music = ["1.mp3", "2.mp3", "3.mp3", "4.mp3", "5.mp3", "6.mp3"];
-
-const playList = document.createElement("audio");
-
-playList.src = `audio/${music}`;
-playList.innerHTML = "controls";
-document.body.appendChild(playList) ;
- 
--- JS/todolist.js
+++ JS/todolist.js
@@ -5,11 +5,11 @@ const TODOS_KEY = "todos"
 let toDos = [];
 
 function saveTodo() {
-  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)); //toDos배열을 string으로 바꿔서 localStorage에 저장
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)); 
 }
 
 function deleteTodo(event) {
-const li = event.target.parentElement; //방금 일어난 이벤트의 객체(button)의 부모요소
+const li = event.target.parentElement; 
 li.remove();
 toDos = toDos.filter((todo) => todo.id !== parseInt(li.id));
 saveTodo();
@@ -19,35 +19,34 @@ function paintTodo(newTodo) {
 const li = document.createElement("li");
 li.id = newTodo.id ;
 const span = document.createElement("span");
-const todoBtn = document.createElement("button")
+const todoBtn = document.createElement("button");
 todoBtn. innerText = "OK";
 li.appendChild(span);
 li.appendChild(todoBtn);
-todoBtn.addEventListener("click", deleteTodo);  //버튼을 누르면 list 삭제하는 함수호출
+todoBtn.addEventListener("click", deleteTodo); 
 span.innerText = newTodo.text;
 todoList.appendChild(li);
 }
 
 function onSubmitEvent(event) {
-  event.preventDefault();   //이벤트의 기본동작 없애기
+  event.preventDefault();  
   const newTodo = todoInput.value;
-  todoInput.value = ""; // 이벤트가 발생시 input의 value값을 안보이게 하기
+  todoInput.value = ""; 
   const newTodoObj = {
     text: newTodo, 
     id: Date.now(),
   };
-  toDos.push(newTodoObj);  // toDos 배열 안에 input의 value값 저장하기
+  toDos.push(newTodoObj); 
   paintTodo(newTodoObj);  
   saveTodo();
 }
 
 todoForm.addEventListener("submit", onSubmitEvent);
 
-const todoArray = localStorage.getItem(TODOS_KEY); //string으로 바꾼 toDos array를 변수에 저장
-
+const todoArray = localStorage.getItem(TODOS_KEY); 
 if(todoArray !== null) {
-  const parsedTodos = JSON.parse(todoArray);//JS가 이해할 수 있는 object로 바꿈
+  const parsedTodos = JSON.parse(todoArray);
   toDos = parsedTodos;
-  parsedTodos.forEach(paintTodo); // object 각각 item에 paintTodo함수 실행 
-}                                  //브라우저를 새로고침 해도 저장된 value가 보임 
+  parsedTodos.forEach(paintTodo);
+}                                  
 
