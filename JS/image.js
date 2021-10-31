47899e5c16384665a48e7dd0acd0a6093c8ed2d4
jw5367 <zzss9921@naver.com>
2021-10-31 19:42:18 +0900

    img src

--- JS/image.js
+++ JS/image.js
@@ -4,5 +4,5 @@ const ranImage = images[Math.floor(Math.random() * images.length)];
 const bgimg = document.createElement("img");
 bgimg.classList.add("universe");
 
-bgimg.src = `./img/${ranImage}`;
+bgimg.src = `C:\Users\김지원\Desktop\Chrome-clone-2021\img\${ranImage}`;
 document.body.appendChild(bgimg) ;
